61d978c3298438103ed6da7bb64ea122c6b52f57
Jeff Spooner <jdspoone@icloud.com>
2022-03-17 11:49:58 -0600

    Initial Commit

---
+++ SwiftUITextFieldAlert/SwiftUITextFieldAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextFieldAlertApp.swift
+//  SwiftUITextFieldAlert
+//
+//  Created by Jeff Spooner on 2022-03-17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextFieldAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
